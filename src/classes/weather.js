3757b24716fb076944fc0b81b6544c6f8b3624bf
Dmitry Osipenko <eympel@gmail.com>
2020-10-25 22:54:33 +0300

    feat: finished taskgit add .!

--- src/classes/weather.js
+++ src/classes/weather.js
@@ -20,12 +20,13 @@ async function getWeather() {
   } else {
     city.textContent = localStorage.getItem('city');
   }
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=en&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=en&appid=c066797bfb27f631c490fd658dadbd74&units=metric`;
   const res = await fetch(url);
   const data = await res.json();
   if (data.cod === '404') {
     displayError(data.message);
     localStorage.setItem('city', localStorage.getItem('oldCity'));
+
     getWeather();
     return;
   }
